52c2aa6ad47991e63c585b7d15469c23ea3eb5fe
Zaher Dirkey <zaherdirkey@yahoo.com>
2015-02-15 00:40:57 +0200

    change license to MIT

--- src/languages/pascal.php
+++ src/languages/pascal.php
@@ -1,29 +1,11 @@
 <?php
-/***********************************************************************
-
-  Copyright (C) 2004  zaher dirkey (zaher@parmaja.com)
-
-  This file is part of .
-
-   is free software; you can redistribute it and/or modify it
-  under the terms of the GNU General Public License as published
-  by the Free Software Foundation;
-
-   is distributed in the hope that it will be useful, but
-  WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
-  MA  02111-1307  USA
-
-************************************************************************
-
-  This syntax class writen by Zaher Dirkey zaher@parmaja.com
-
-************************************************************************/
+/**
+*   This file is part of the "SARD"
+*
+*   @license   The MIT License (MIT) Included in this distribution
+*   @author    Zaher Dirkey <zaher at yahoo dot com>
+*
+*/
 
   class pascal_syn extends plain_code_syn
   {
